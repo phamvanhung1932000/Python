e9c9e97a93e9c0cc2d27b6ab24b489f8f1de84e1
phamvanhung1932000 <61811511+phamvanhung1932000@users.noreply.github.com>
2020-04-05 15:15:32 +0700

    Add files via upload

---
+++ Giải phương trình bậc nhất ( trang 76 ).py
@@ -0,0 +1,15 @@
+# In chuỗi Hello
+print('Hello')
+print(' Đây là chương trình giải phương trình bậc nhất có dạng: ax + b = 0 ')
+
+# Nhập a
+a = int(input('a = '))
+        
+# Nhập b        
+b = int(input('b = '))
+        
+# Gán x cho giá trị -b/a        
+x = -b/a
+        
+# In giá trị x ra màn hình
+print(' x = ' + str(x))
---
+++ Viết chương trình hiện thị các nội dung ( trang 113 ).py
@@ -0,0 +1,3 @@
+# Viết chương trình hiện thị các nội dung
+print("Hello, world!")
+print("Phạm Văn Hùng, 18575202160032, KTĐK&TĐH")
